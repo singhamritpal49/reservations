b765a7119e2d98cec9085cb128de8ee13846aaee
Your Name <you@example.com>
2019-07-30 10:56:21 -0400

    added models

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   resources :users
-=======
+
   resources :restaurants
->>>>>>> Irina
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
