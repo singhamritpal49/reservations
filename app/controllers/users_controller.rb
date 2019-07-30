a86e048e5e11a0ea3c605c7c986bdc3b4d78c8f4
Your Name <you@example.com>
2019-07-30 18:12:56 -0400

    Made USER CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,43 @@ def index
   @users = User.all
 end
 
+def show
+  @user = User.find(params[:id])
+end
+
+def new
+  @user = User.new
+end
+
+
+def create
+  @user = User.create(user_params)
+  redirect_to @user
+end
+
+def edit
+  @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+  @user.update(user_params)
+  redirect_to @user
+end
+
+def destroy
+  @user = User.find(params[:id])
+  @user.destroy
+  redirect_to users_path
+end
+
+
+#private Method
+def user_params
+  params.require(:user).permit(:name, :phone)
+
+end
+
+
+
 end
