913ac2e40876ccedcb2ce7b4749ba3807898ec03
github username <github email address>
2019-07-30 10:40:02 -0400

    added reservation join model

---
+++ db/migrate/20190730143841_create_reservations.rb
@@ -0,0 +1,11 @@
+class CreateReservations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reservations do |t|
+      t.string :type
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :restaurant, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
